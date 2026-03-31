49a03e7c38605505296bff63a2826c8e1d1664de
manuthkausilu <manuthkausilu20031018@gmail.com>
2026-03-31 22:20:27 +0530

    add ecosystem configuration for customer, product, and order services

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  apps: [
+    // {
+    //   name: "cloud-sql-auth-proxy",
+    //   script: " ./cloud-sql-proxy project-9bb76dab-712f-4e59-b4e:asia-southeast1:postgres-vm project-9bb76dab-712f-4e59-b4e:asia-southeast1:mysql-vm --private-ip",
+    //   log_file: "./logs/cloud-sql-auth-proxy.log",
+    // },
+    {
+      name: "customer-service",
+      script: "java -jar ./customer-service/target/Customer-Service-1.0.0.jar",
+      log_file: "./logs/customer-service.log",
+      instances:2
+    },
+    {
+      name: "product-service",
+      script: "java -jar ./product-service/target/Product-Service-1.0.0.jar",
+      log_file: "./logs/product-service.log",
+      instances:2
+    },
+    {
+      name: "order-service",
+      script: "java -jar ./order-service/target/Order-Service-1.0.0.jar",
+      log_file: "./logs/order-service.log",
+      instances:2
+    }
+  ]
+}
